adf4c496bcb667c7709e0caef00f4037e96f7713
niteoliveira <142188575+niteoliveira@users.noreply.github.com>
2025-12-07 20:49:59 -0300

    ajustando arquivo csv_to_json.py

--- scripts/csv_to_json.py
+++ scripts/csv_to_json.py
@@ -4,9 +4,11 @@ import json
 INPUT_CSV = "data/credit-card2.csv"
 OUTPUT_JSON = "data/credit-card2.json"
 
-with open(INPUT_CSV, model="r", newline="", encoding="utf-8") as csvfile:
-	reader = csv.DictReader(csvfile)
-	data = list(reader)
+with open(INPUT_CSV, mode="r", newline="", encoding="utf-8") as csvfile:
+    reader = csv.DictReader(csvfile)
+    data = list(reader)
 
-with open(OUTPUT_JSON, mode="w", encoding="utf-8") as jsonfile;
-	json.dump(data, jsonfile, indent=2)
+with open(OUTPUT_JSON, mode="w", encoding="utf-8") as jsonfile:
+    json.dump(data, jsonfile, indent=2)
+
+print(f"Convertido {INPUT_CSV} -> {OUTPUT_JSON} com {len(data)} registros.")
