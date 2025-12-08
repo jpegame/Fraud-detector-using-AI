df2c4c51f169142c7a1e46f8f2338db8629bf0fb
niteoliveira <142188575+niteoliveira@users.noreply.github.com>
2025-12-07 22:27:27 -0300

    Add script to import JSON data into MongoDB

---
+++ scripts/import_json.py
@@ -0,0 +1,15 @@
+from pymongo import MongoClient
+import json
+
+URI = "mongodb+srv://Aluno:aluno123@clusterlineu.b5nhvya.mongodb.net/?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true"
+
+client = MongoClient(URI, tls=True, tlsAllowInvalidCertificates=True)
+
+db = client["meuBdTeste"]          # cria/usa o banco
+collection = db["transacoes"]      # cria/usa a coleção
+
+with open("data/credit-card2.json", "r") as f:
+    docs = json.load(f)
+
+result = collection.insert_many(docs)
+print(f"Inseridos {len(result.inserted_ids)} documentos")
