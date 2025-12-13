cc5bab514c1885e74858b5a6e09cf43d843eb6ca
Rafael Mota <rafaelssoni1000@gmail.com>
2025-12-13 20:42:01 -0300

    Test

--- scripts/silver_processing.py
+++ scripts/silver_processing.py
@@ -1,17 +1,19 @@
 import pandas as pd
 from db import get_mysql_engine
+from sqlalchemy import text
 
 def run_silver():
     engine = get_mysql_engine()
 
-    df = pd.read_sql(
-        "SELECT * FROM credit_card_transactions",
-        engine
-    )
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM credit_card_transactions"))
+        rows = result.fetchall()
+        columns = result.keys()
+
+    df = pd.DataFrame(rows, columns=columns)
 
     df = df.drop_duplicates()
     df = df.dropna()
 
     print(f"[SILVER] DataFrame criado com {len(df)} registros")
-
     return df
